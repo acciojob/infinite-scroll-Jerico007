c936f98951273e39002268fef807d57cf1bae851
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-09-20 12:32:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here!
+let ol = document.getElementById("infi-list");
+let count = 1;
 
+
+function loadContent(num)
+{
+	for(let i= 1; i <= num;i++)
+		{
+			let li = document.createElement("li");
+			li.innerText = `Item ${count}`;
+		   ol.appendChild(li);
+			count++;
+		}
+}
+
+loadContent(10);
+
+ol.addEventListener("scroll",(event)=>{
+ console.log("scrolled", window.scrollY) //scrolled from top
+    console.log(window.innerHeight) //visible part of screen
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+      			let li = document.createElement("li");
+			li.innerText = `Item ${count}`;
+		   ol.appendChild(li);
+			count++;
+    }
+})
